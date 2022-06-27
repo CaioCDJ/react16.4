13d0380f1678001113341eae2b90f5b086b62628
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-27 17:18:06 -0300

    multiples exports and imports

---
+++ src/components/Bomdia.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>
+<>
+  <h1>bom dia {props.nome}</h1>
+  <h2>Até breve!</h2>
+
+</>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import  Reat from 'react';
 import ReactDOM from 'react-dom';
 
-import Primeiro from './components/Primeiro';
+//import Primeiro from './components/Primeiro';
+//import BomDia from './components/Bomdia.jsx';
+import Multi from './components/Multiplos';
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Multi.BoaTarde nome="Ana" />
+    <Multi.BoaNoite nome="Bia"/>
+  </div>
+  , document.getElementById('root'));
