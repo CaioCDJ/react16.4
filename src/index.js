fc33c86151e9f159dcb821046a69e28aee890cf4
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-28 13:15:17 -0300

    child component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import  Reat from 'react';
+import  React from 'react';
 import ReactDOM from 'react-dom';
 
 //import Primeiro from './components/Primeiro';
 //import BomDia from './components/Bomdia.jsx';
 //import Multi from './components/Multiplos';
-import Saudacao from './components/Saudacao';
+//import Saudacao from './components/Saudacao';
+import Pai from './components/Pai';
+import Filho from './components/Filho';
+
 
 ReactDOM.render(
   <div>
-    <Saudacao tipo="Bom dia" nome="Jõao"/>
+    <Pai nome="Paulo" sobrenome="Souza">
+      <Filho nome="Jose"/>
+      <Filho nome="Pedro"/>
+      <Filho nome="Carlos"/>
+    </Pai>
   </div>
   , document.getElementById('root'));
